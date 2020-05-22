ac6e7e4986ddc7d80a584cead3f9262450d38960
Philipp Nowak <git@lit.plus>
2020-05-22 04:10:25 +0200

    Add sudo to wheel group

--- chrootsetup.sh
+++ chrootsetup.sh
@@ -45,6 +45,16 @@ confirmbefore systemctl disable systemd-networkd.service \
   \&\& pacman -Sy --needed networkmanager
 
 psec "Setting up user account"
+pask "Setting up sudo for wheel group"
+TMP_SUDOERS=$(mktemp)
+destcmd cp /etc/sudoers "$TMP_SUDOERS"
+destcmd sed -i -e 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' "$TMP_SUDOERS"
+if ! visudo -c -f "$TMP_SUDOERS"; then
+  perr "Modified sudoers file not longer validates."
+  exit 1
+fi
+cp /etc/sudoers /etc/sudoers.bkp
+mv "$TMP_SUDOERS" /etc/sudoers
 pask "What is the primary username for this system?"
 read un
 if ! id $un; then
