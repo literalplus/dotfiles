08120b5051c3df92ce590fe153b8e4cb058e3c30
Philipp Nowak <philipp.nowak@cloudflight.io>
2020-07-31 12:26:15 +0200

    Add cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# For $HOME/.gitconfig: (Credit mschreiber)
+#[alias]
+#    cleanup = "!git branch --merged | grep -v '^  master$' | grep -v '^  development$' | grep -v '^* ' | xargs git branch -d"
+#    force-cleanup = "!git branch | grep -v '^  master$' | grep -v '^  development$' | grep -v '^* ' | xargs git branch -D"
+
+CLEANUP_OP="cleanup"
+
+if [[ "$1" == "tomaster" ]]; then
+  TOMASTER="yes"
+fi
+
+if [[ "$1" == "extremeyeetmode" ]]; then
+  TOMASTER="yes"
+  CLEANUP_OP="force-cleanup"
+fi
+
+function cleanrepo() {
+  if [[ -d ".git" ]]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD | tr -d '\n')
+    echo " ... Processing ${PWD##*/} at branch $BRANCH"
+    if [[ "$TOMASTER" == "yes" ]]; then
+      if git rev-parse --verify development >/dev/null 2>&1; then
+        if [[ "$BRANCH" != "development" ]]; then
+          echo " Checking out development"
+          git checkout development
+        fi
+      elif [[ "$BRANCH" != "master" ]]; then
+        echo " Checking out master"
+        git checkout master
+      fi
+    fi
+    git $CLEANUP_OP
+  else
+    echo " --- Not a git repository: ${PWD##*/}"
+  fi
+}
+
+for dirname in ./*/; do
+  pushd "$dirname" >/dev/null || exit
+  cleanrepo
+  popd >/dev/null || exit
+done
+
+# Also try current directory
+cleanrepo
